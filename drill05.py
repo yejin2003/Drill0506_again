98fa3b7ea03537fb32be8e243d6075aef798e897
yejin2003 <orangered1111@gmail.com>
2023-10-02 20:44:40 +0900

    반복적으로 만들어지는 랜덤 손 화살표 위치로 캐릭터 이동 구현 완료. t값이 1을 넘으면 다시, 새로운 손 화살표가 랜덤 위치에 만들어짐

--- drill05.py
+++ drill05.py
@@ -7,7 +7,7 @@ TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 def load_resources():
     global TUK_ground, character
     global arrow
-    arrow=load_image('hand_arrow.png')
+    arrow = load_image('hand_arrow.png')
     TUK_ground = load_image('TUK_GROUND.png')
     character = load_image('animation_sheet.png')
 
@@ -25,25 +25,31 @@ def handle_events():
 
 def reset_world():
     global running, cx, cy, frame
-    global hx,hy
-    global sx,sy
+    global hx, hy
+    global sx, sy
     global t
     global action
 
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
-    action=3
+    action = 3
 
-    sx,sy=cx,cy # p1:시작점
+    set_new_target_arrow()
+
+
+def set_new_target_arrow():
+    global sx, sy, hx, hy, t
+    sx, sy = cx, cy  # p1:시작점
     # hx,hy=50,50
-    hx,hy=random.randint(0,TUK_WIDTH),random.randint(0,TUK_HEIGHT) # p2:끝점
-    t=0.0
+    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2:끝점
+    t = 0.0
+
 
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    arrow.draw(hx,hy)
+    arrow.draw(hx, hy)
     character.clip_draw(frame * 100, 100 * action, 100, 100, cx, cy)
     update_canvas()
 
@@ -51,18 +57,22 @@ def render_world():
 def update_world():
     global t
     global frame
-    global cx,cy
+    global cx, cy
     global action
 
     frame = (frame + 1) % 8
-    action=1 if cx<hx else 0
-    if cx<hx:
-        action=1
+    action = 1 if cx < hx else 0
+    if cx < hx:
+        action = 1
 
-    if t<1.0:
-        cx=(1-t)*sx+t*hx # cx는 시작 x와 끝 x를 1-t:t의 비율로 섞은 위치
-        cy=(1-t)*sy+t*hy
-        t+=0.001
+    if t < 1.0:
+        cx = (1 - t) * sx + t * hx  # cx는 시작 x와 끝 x를 1-t:t의 비율로 섞은 위치
+        cy = (1 - t) * sy + t * hy
+        t += 0.001
+    else:
+        cx,cy=hx,hy #캐릭터 위치를 목적지 위치와 강제로 정확히 일치시킴
+        set_new_target_arrow()
+
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
@@ -71,8 +81,8 @@ load_resources()
 reset_world()
 
 while running:
-    render_world() # 월드의 현재 내용을 그린다.
-    update_world() # 사용자의 입력을 받아들인다.
-    handle_events() # 빌드 안의 객체들의 상호작용을 계산하고 그 결과를 update한다.
+    render_world()  # 월드의 현재 내용을 그린다.
+    update_world()  # 사용자의 입력을 받아들인다.
+    handle_events()  # 빌드 안의 객체들의 상호작용을 계산하고 그 결과를 update한다.
 
 close_canvas()
